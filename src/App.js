fef90fca837041c848d28200d46e2249109d1137
Ethan Wiegert <ewiegert99@gmail.com>
2023-08-29 17:58:58 -0400

    Created basic Footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from "./Header"
 import Story from './Story';
+import Footer from './Footer';
 
 function App() {
   return (
     <>
    <Header/>
    <Story/>
+   <Footer/>
    </>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+function Footer(){
+return (
+    <div className="row">
+        <div className="d-flex justify-content-center fixed-bottom text-light bg-dark">
+        <footer>
+            <h6>Made By Ethan Wiegert</h6>
+            </footer>
+            </div>
+            </div>
+)
+}
+
+export default Footer
